e37323c791a302bd58472dd93785da258b9df0ca
PauloMarvin <paulo.marving@gmail.com>
2024-05-05 18:37:06 -0300

    feat: bayesian classifier

--- classifiers/bayesian_classifier.py
+++ classifiers/bayesian_classifier.py
@@ -4,29 +4,29 @@ import pandas as pd
 
 class BayesianGaussianClassifier:
     def __init__(self):
-        self.classes = None
+        self.class_names = None
         self.class_means = None
         self.class_covs = None
 
     def fit(self, X_train: pd.DataFrame, y_train: np.ndarray | pd.Series) -> None:
-        self.classes = np.unique(y_train)
+        self.class_names = np.unique(y_train)
         self.class_means = {}
         self.class_covs = {}
 
-        for c in self.classes:
-            X_c = X_train[y_train == c]
+        for class_name in self.class_names:
+            X_train_class = X_train[y_train == class_name]
 
-            mean = np.mean(X_c, axis=0)
-            cov = np.cov(X_c, rowvar=False)
+            mean = np.mean(X_train_class, axis=0)
+            cov = np.cov(X_train_class, rowvar=False)
 
-            self.class_means[c] = mean
-            self.class_covs[c] = cov
+            self.class_means[class_name] = mean
+            self.class_covs[class_name] = cov
 
     def _predict_proba(self, X_test: np.ndarray) -> np.ndarray:
         probabilities = []
-        for c in self.classes:
-            mean = self.class_means[c].values
-            cov = self.class_covs[c]
+        for class_name in self.class_names:
+            mean = self.class_means[class_name].values
+            cov = self.class_covs[class_name]
             prob = self.multivariate_normal_pdf(X_test, mean, cov)
             probabilities.append(prob)
         return np.array(probabilities).T
