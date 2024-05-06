8aa0cff51f6106ccd4bcebd59f4c2be842878ac9
PauloMarvin <paulo.marving@gmail.com>
2024-05-06 12:00:50 -0300

    feat: add possibility of normalization in the Baynesian classifier

--- classifiers/bayesian_classifier.py
+++ classifiers/bayesian_classifier.py
@@ -3,10 +3,11 @@ import pandas as pd
 
 
 class BayesianGaussianClassifier:
-    def __init__(self):
+    def __init__(self, normalization: bool = False) -> None:
         self.class_names = None
         self.class_means = None
         self.class_covs = None
+        self.normalization = normalization
 
     def fit(self, X_train: pd.DataFrame, y_train: np.ndarray | pd.Series) -> None:
         self.class_names = np.unique(y_train)
@@ -17,19 +18,15 @@ class BayesianGaussianClassifier:
             X_train_class = X_train[y_train == class_name]
 
             mean = np.mean(X_train_class, axis=0)
-            cov = np.cov(X_train_class, rowvar=False)
+            if self.normalization:
+                cov = self._regularize_covariance_matrix(
+                    np.cov(X_train_class, rowvar=False)
+                )
+            else:
+                cov = np.cov(X_train_class, rowvar=False)
 
             self.class_means[class_name] = mean
             self.class_covs[class_name] = cov
-
-    def _predict_proba(self, X_test: np.ndarray) -> np.ndarray:
-        probabilities = []
-        for class_name in self.class_names:
-            mean = self.class_means[class_name].values
-            cov = self.class_covs[class_name]
-            prob = self.multivariate_normal_pdf(X_test, mean, cov)
-            probabilities.append(prob)
-        return np.array(probabilities).T
 
     def multivariate_normal_pdf(
         self, X: np.ndarray, mean: np.ndarray, cov: np.ndarray
@@ -48,3 +45,20 @@ class BayesianGaussianClassifier:
     def predict(self, X_test: np.ndarray) -> np.ndarray:
         probabilities = self._predict_proba(X_test)
         return np.argmax(probabilities, axis=1)
+
+    def _regularize_covariance_matrix(
+        self, covariance_matrix: np.ndarray, lambda_: float = 1e-3
+    ) -> np.ndarray:
+        regularized_covariance_matrix = covariance_matrix + lambda_ * np.identity(
+            covariance_matrix.shape[0]
+        )
+        return regularized_covariance_matrix
+
+    def _predict_proba(self, X_test: np.ndarray) -> np.ndarray:
+        probabilities = []
+        for class_name in self.class_names:
+            mean = self.class_means[class_name].values
+            cov = self.class_covs[class_name]
+            prob = self.multivariate_normal_pdf(X_test, mean, cov)
+            probabilities.append(prob)
+        return np.array(probabilities).T
